029564f09f7d5beb0211eb75193289be2db2794a
Dimitrios Athanasiadis <dimitrioca@csd.auth.gr>
2021-12-06 13:48:03 +0200

    Finished searchReservations of AdminUI.java

--- src/AdminUI.java
+++ src/AdminUI.java
@@ -1,54 +1,160 @@
+import java.text.DecimalFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
 
 public class AdminUI {
     private Map<String,Provider> providers;
     private Map<Integer,Reservation> reservations;
+    private final Map<Integer,Room> rooms;
     private Map<String,Customer> customers;
     private Map<String,Admin> admins;
+    private Map<String,Authentication> users;
+    private static DecimalFormat df;
     Admin admin;
     Scanner scanner=new Scanner(System.in);
     MainUI mainUI;
 
     /**
-     *
-     * @param mainUI
-     * @param admin
-     * @param reservations
-     * @param customers
-     * @param providers
+     *  Constructor of AdminUI
      */
     public AdminUI(MainUI mainUI, Admin admin, Map<Integer,Reservation> reservations, Map<String, Customer> customers,
-                   Map<String,Provider> providers,Map<String,Admin> admins){
+                   Map<String,Provider> providers,Map<String,Admin> admins,Map<String,Authentication> users, Map<Integer,Room> rooms){
         this.admin = admin;
+        this.rooms = rooms;
         this.mainUI = mainUI;
         this.reservations=reservations;
         this.customers=customers;
         this.providers=providers;
         this.admins=admins;
+        this.users = users;
+        df = new DecimalFormat("0.00");
         panel();
     }
 
     /**
-     * this function searches and shows all the reservations
+     * This function reads an integer from user and returns it. If the input is
+     * invalid it returns 0.
+     * @return  the entered integer
+     */
+    public int scanInput(){
+        int input = 0;
+        System.out.print("\n> ");
+        try {
+            input = scanner.nextInt();
+        }
+        catch (java.util.InputMismatchException ignored){scanner.nextLine();}
+        return input;
+    }
+
+    public void searchReservationsByUsername(String username){
+        AtomicBoolean roomFound = new AtomicBoolean(false);
+        AtomicInteger counter = new AtomicInteger(1);
+        this.customers.get(username).getReservationIDs().forEach((reservationID) -> {
+            System.out.println(counter + ". " + "reservation ID: " + reservations.get(reservationID).getReservationID()
+                    + ", Username: \"" + reservations.get(reservationID).getUsername() + "\", Guests: "
+                    + reservations.get(reservationID).getGuestNumber() + ", Check in: "
+                    + reservations.get(reservationID).getCheckIn().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+                    + ", Check out: " + reservations.get(reservationID).getCheckOut().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+                    + ", Total price: $" + df.format((reservations.get(reservationID).getTotalPrice())));
+            roomFound.set(true);
+            counter.getAndIncrement();
+        });
+        if (!roomFound.get()) {
+            System.out.println("\nNo reservations found for username: " + username);
+        }
+    }
+
+    public void searchReservationsByRoomID(int roomID){
+        System.out.println();
+        AtomicBoolean roomFound = new AtomicBoolean(false);
+        AtomicInteger counter = new AtomicInteger(1);
+        reservations.forEach((id, reservation) -> {
+            if (reservation.getRoomID() == roomID) {
+                System.out.println(counter + ". " + "reservation ID: " + reservation.getReservationID() + ", Username: \""
+                        + reservation.getUsername() + "\", Guests: " + reservation.getGuestNumber() + ", Check in: "
+                        + reservation.getCheckIn().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+                        + ", Check out: " + reservation.getCheckOut().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+                        + ", Total price: $" + df.format((reservation.getTotalPrice())));
+                roomFound.set(true);
+                counter.getAndIncrement();
+            }
+        });
+        if (!roomFound.get()) {
+            System.out.println("\nNo reservations found for this room");
+        }
+    }
+
+
+    public void returnAllReservations(){
+        var ref = new Object() {
+            int c = 1;
+        };
+        reservations.forEach((reservationID, reservation) -> {
+            System.out.println(ref.c + ". " + "reservation ID: " + reservation.getReservationID() + ", Username: \""
+                    + reservation.getUsername() + "\", Guests: " + reservation.getGuestNumber() + ", Check in: "
+                    + reservation.getCheckIn().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+                    + ", Check out: " + reservation.getCheckOut().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+                    + ", Total price: $" + df.format((reservation.getTotalPrice())));
+            ref.c++;
+        });}
+
+    /**
+     *    search reservations by username, by room identifier or return all of them
      */
     public void searchReservations(){
-
         System.out.println("\n+============================+");
         System.out.println("|  Search/Show Reservations   |");
-        System.out.println("\n+============================+");
+        System.out.println("+============================+");
+        int cmd=0;
+        boolean validInput=false;
 
-        for (Map.Entry<Integer, Reservation> entry : this.reservations.entrySet()) {
-            Integer reservationID = entry.getKey();
-            Reservation value = entry.getValue();
-            System.out.println(new StringBuilder().append("id:").append(value.getReservationID()).append(",guests:").
-                    append(value.getGuestNumber()).append(",Nights:").append(value.getTotalNights()).append(",Room's Id:")
-                    .append(value.getRoomID()).append(",Check in:").append(value.getCheckIn()).append(",check out:").append(value.getCheckOut())
-                    .append(",Customer's username:").append(value.getUsername()).toString());
+        while (!validInput){
+            System.out.println("\n1. Search by customer username\n2. Search by room ID\n3. Return all reservations");
+            cmd = scanInput();
+            switch (cmd) {
+                case 1 -> {
+                    validInput = true;
+                    System.out.println("\nEnter the customer username to search reservations for:");
+                    System.out.print("\n> ");
+                    String username="";
+                    try {
+                        username = scanner.next();
+                    }
+                    catch (java.util.InputMismatchException ignored){scanner.nextLine();}
+                    if (!this.customers.containsKey(username)){
+                        scanner.nextLine();
+                        System.out.println("\nUsername \"" + username + "\" not found");
+                    }
+                    else {searchReservationsByUsername(username);}
+                }
+                case 2 -> {
+                    validInput = true;
+                    System.out.println("\nEnter the room ID to search reservations for:");
+                    System.out.print("\n> ");
+                    int roomID = 0;
+                    try {roomID = scanner.nextInt();}
+                    catch (java.util.InputMismatchException ignored){
+                        scanner.nextLine();
+                    }
+                    if (!this.rooms.containsKey(roomID)){
+                        if (roomID!=0) {System.out.println("\nRoom with id " + roomID + " not found");}
+                        else {System.out.println("\nInvalid input. Enter a valid room ID");}
+                    }
+                    else if (roomID>0) {searchReservationsByRoomID(roomID);}
+                }
+                case 3 -> {
+                    validInput = true;
+                    System.out.println("\nReturning all reservations\n");
+                    returnAllReservations();
+                }
+                default -> System.out.println("\nInvalid input, enter a valid number");
+            }
         }
-
-
     }
 
     /**
@@ -136,19 +242,18 @@ public class AdminUI {
      *   The main provider user interface. It asks for a command and calls the
      *   appropriate function.
      */
-
     public void panel(){{
             while (true){
-                System.out.println("\n+============================+");
-                System.out.println("|       Admin    Panel       |");
-                System.out.println("+============================+");
-                System.out.println("| 1. Search/Show reservations|");
-                System.out.println("| 2. Search/Show users       |");
-                System.out.println("| 3. Approve new user        |");
-                System.out.println("| 4. Send message to user    |");
-                System.out.println("| 5. Log out                 |");
-                System.out.println("| 6. Exit                    |");
-                System.out.println("+============================+");
+                System.out.println("\n+=============================+");
+                System.out.println("|         Admin Panel         |");
+                System.out.println("+=============================+");
+                System.out.println("| 1. Search/Show reservations |");
+                System.out.println("| 2. Search/Show users        |");
+                System.out.println("| 3. Approve new user         |");
+                System.out.println("| 4. Send message to user     |");
+                System.out.println("| 5. Log out                  |");
+                System.out.println("| 6. Exit                     |");
+                System.out.println("+=============================+");
                 int input = 0;
                 System.out.print("\n> ");
                 try {
--- src/MainUI.java
+++ src/MainUI.java
@@ -63,6 +63,7 @@ public class MainUI {
         this.rooms.put(3,new Room(3, "hotel2", "hotel",false,4, 30, 35, false, true, false, true, true, false, true, false, false));
         this.rooms.put(4,new Room(4, "room1", "room",false,3, 25, 35, false, true, false, true, true, false, false, false, false));
         this.reservations.put(1,new Reservation(1,2, 3, 3, LocalDate.of(2021, 3, 5), LocalDate.of(2021, 3, 7), "user2", 50));
+        this.reservations.put(2,new Reservation(2,1, 3, 2, LocalDate.of(2021, 6, 15), LocalDate.of(2021, 7, 17), "user1", 40));
     }
 
     /**
@@ -101,7 +102,7 @@ public class MainUI {
                 ProviderUI providerUI = new ProviderUI(this.mainUI, this.providers.get(username), this.rooms, this.reservations);
             }
             case 3 -> {
-                AdminUI adminUI = new AdminUI(this.mainUI, this.admins.get(username),this.reservations,this.customers,this.providers,this.admins);
+                AdminUI adminUI = new AdminUI(this.mainUI, this.admins.get(username),this.reservations,this.customers,this.providers,this.admins,this.users,this.rooms);
             }
             default -> System.exit(0);
         }
